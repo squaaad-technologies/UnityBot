4082207ac88076ee9c5073a8e2fa0b718b8e38c5
Jai L <47060357+jdadonut@users.noreply.github.com>
2020-10-18 00:03:45 -0400

    cummies

--- src/bot/bot.js
+++ src/bot/bot.js
@@ -1,1 +1,20 @@
 //uwu
+const CommandsHandler = require('./packages/CommandsHandler.js');
+const Discord = require('discord.js');
+const ServerSettings = require('./packages/ServerSettingsHandler.js')
+
+const Client = new Discord.Client();
+Client.SettingsManager = new ServerSettings(require(require('path').join(__dirname, 'config.json')), require('path').join(__dirname, 'config.json'));
+
+Client.login(process.env.botToken || Client.SettingsManager.get('client','token'));
+Client.commands = new CommandsHandler(require('path').join(__dirname,'commands'));
+Client.on('message', message => {
+        if(message.author.bot){return;};
+        var prefix = Client.SettingsManager.get(message.guild.id, 'prefix') || Client.SettingsManager.defaults.prefix;
+        const args = message.content.slice(prefix.length).trim().split(/ +/);
+        const command = args.shift().toLowerCase();
+        if(Client.commands.has(command)){try{Client.commands.get(command).execute(message,args,Client,)} catch (err) {console.log(err); message.reply('There was an uncaught error executing that command! Tell the devs (<@!98133204636028928> because dont ping mods) for help!')}};
+        });
+Client.on('guildCreate', guild => {Client.SettingsManager.joinedServer(guild.id)});
+
+
--- src/bot/packages/CommandsHandler.js
+++ src/bot/packages/CommandsHandler.js
@@ -3,13 +3,14 @@ class CommandsHandler extends Discord.Collection{
     constructor(dir) {
         super(); // owo
         this._dir = dir;
-        this.commands.forEach(command => this.assignCommand(command.name, command))
+        this.commands.forEach(command => this.assignCommand(require(String(require('path').join(this._dir,command))).name, require(String(require('path').join(this._dir,command)))));
     }
     get commands(){
         return require('fs').readdirSync(this._dir).filter(file => file.endsWith('.js') && !file.includes('.disabled'));
     }
     assignCommand(name, command){
+        console.log(`${name || "name undefined"}, ${command || "command undefined"}`)
         this.set(name, command);
     }
 }
-
+module.exports = CommandsHandler
